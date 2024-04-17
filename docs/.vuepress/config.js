26177ccb0b74995e39a991f3190cf4aaeac52b0e
Yonatan Kof <kof@yonatankof.com>
2024-04-17 20:19:48 +0300

    Update VuePress config.js

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,18 +1,32 @@
-import { defaultTheme } from '@vuepress/theme-default'
-import { defineUserConfig } from 'vuepress/cli'
-import { viteBundler } from '@vuepress/bundler-vite'
+import { defaultTheme } from '@vuepress/theme-default';
+import { defineUserConfig } from 'vuepress/cli';
+import { viteBundler } from '@vuepress/bundler-vite';
 
 export default defineUserConfig({
-  lang: 'en-US',
+	lang: 'en-US',
 
-  title: 'Pepperi Brand',
-  description: "A place to find Pepperi's brand guidelines, download brand assets, and review user persona",
+	title: 'Pepperi Brand',
+	description: "A place to find Pepperi's brand guidelines, download brand assets, and review user persona",
+  head: [['link', { rel: 'icon', href: '/images/pepperi-favicon.ico' }]],
 
-  theme: defaultTheme({
-    logo: 'https://yonatankof.com/misc/pepp/Pepperi-Icon-Green.svg',
+	theme: defaultTheme({
+		logo: '/images/Pepperi-Icon.svg',
 
-    navbar: ['/', '/get-started'],
-  }),
+		navbar: ['/', '/get-started', '/visual-identity/index.html'],
+		sidebar: {
+			'/visual-identity/': [
+				{
+					text: 'Visual Identity',
+					children: [
+						'/visual-identity/logo-variations.md',
+						'/visual-identity/color-palette.md',
+						'/visual-identity/typography.md',
+						'/visual-identity/imagery-photography.md',
+					],
+				},
+			],
+		},
+	}),
 
-  bundler: viteBundler(),
-})
+	bundler: viteBundler(),
+});
