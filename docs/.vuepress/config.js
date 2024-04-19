7dbe63c9ff8f13022897cf97865d992d19e24549
Yonatan Kof <kof@yonatankof.com>
2024-04-19 15:54:17 +0300

    Remove dark mode toggle

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -9,12 +9,12 @@ const __dirname = getDirname(import.meta.url)
 
 export default defineUserConfig({
 	lang: 'en-US',
-
 	title: 'Pepperi Brand',
 	description: "A place to find Pepperi's brand guidelines, download brand assets, and review user persona",
 	head: [['link', { rel: 'icon', href: '/images/pepperi-favicon.ico' }]],
-
+	
 	theme: defaultTheme({
+		colorModeSwitch: false,
 		logo: '/images/Pepperi-Icon.svg',
 
 		navbar: ['/', '/get-started', '/visual-identity/index.html'],
